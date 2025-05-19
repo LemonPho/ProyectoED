ec9b36591d83f0dd046bab0d03f1e0880a6063ed
BenitoCamelo2 <louismatthew1112@gmail.com>
2025-05-18 21:29:27 -0600

    moved meterstotimewalked to util

--- src/util.cpp
+++ src/util.cpp
@@ -11,3 +11,16 @@ void util::ClearBuffer(){
     int c;
     while ((c = getchar()) != '\n' && c != EOF);
 }
+
+std::string util::MetersToWalkingTime(double meters) {
+    double walkingSpeed = 1.4; // meters per second
+    int totalSeconds = static_cast<int>(std::ceil(meters / walkingSpeed));
+
+    int minutes = totalSeconds / 60;
+    int seconds = totalSeconds % 60;
+
+    std::ostringstream oss;
+    oss << std::setw(2) << std::setfill('0') << minutes << ":"
+        << std::setw(2) << std::setfill('0') << seconds;
+    return oss.str();
+}
